f7942f582970efee4718b2f88cfa0fcbcd805d20
kez <kerrialbeckettnewham@gmail.com>
2023-04-19 19:01:53 +0200

    init commit

---
+++ src/Controller/AppController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AppController extends AbstractController
+{
+
+    #[Route(path: '/', name: 'app_index')]
+    public function index() : Response
+    {
+        return $this->render('app/index.html.twig');
+    }
+
+    #[Route(path: '/services', name: 'services')]
+    public function services() : Response
+    {
+        return $this->render('app/services.html.twig');
+
+    }
+
+    #[Route(path: '/about', name: 'about')]
+    public function about() : Response
+    {
+        return $this->render('app/about.html.twig');
+    }
+
+    #[Route(path: '/contact', name: 'contact')]
+    public function contact() : Response
+    {
+        return $this->render('app/contact.html.twig');
+    }
+
+}
