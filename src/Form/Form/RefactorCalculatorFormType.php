93d16376505f0a9b7120accb469a99cd1af08ad8
kez <kerrialbeckettnewham@gmail.com>
2024-12-17 13:26:06 +0100

    add deployment | add workflow | add cs

--- src/Form/Form/RefactorCalculatorFormType.php
+++ src/Form/Form/RefactorCalculatorFormType.php
@@ -4,17 +4,12 @@ declare(strict_types=1);
 
 namespace App\Form\Form;
 
-use App\Entity\Feedback;
-use App\Enum\FeedbackEnum;
 use App\Model\RefactorModel;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\EnumType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class RefactorCalculatorFormType extends AbstractType
 {
