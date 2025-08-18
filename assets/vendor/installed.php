ef7e7ba5e816116b9d9d1453fb772f9a1853a0e4
kez <kerrialbeckettnewham@gmail.com>
2025-08-18 12:08:21 +0200

    add assets

---
+++ assets/vendor/installed.php
@@ -0,0 +1,171 @@
+<?php return array (
+  '@hotwired/stimulus' => 
+  array (
+    'version' => '3.2.2',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@hotwired/turbo' => 
+  array (
+    'version' => '7.3.0',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'typed.js' => 
+  array (
+    'version' => '2.1.0',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@stimulus-components/clipboard' => 
+  array (
+    'version' => '5.0.0',
+    'dependencies' => 
+    array (
+      0 => '@hotwired/stimulus',
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@stimulus-components/scroll-progress' => 
+  array (
+    'version' => '5.0.0',
+    'dependencies' => 
+    array (
+      0 => '@hotwired/stimulus',
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'bootstrap' => 
+  array (
+    'version' => '5.3.3',
+    'dependencies' => 
+    array (
+      0 => '@popperjs/core',
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@popperjs/core' => 
+  array (
+    'version' => '2.11.8',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'bootstrap/dist/css/bootstrap.min.css' => 
+  array (
+    'version' => '5.3.3',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'flowbite' => 
+  array (
+    'version' => '3.1.2',
+    'dependencies' => 
+    array (
+      0 => '@popperjs/core',
+      1 => 'flowbite-datepicker',
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'flowbite-datepicker' => 
+  array (
+    'version' => '1.3.2',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'flowbite/dist/flowbite.min.css' => 
+  array (
+    'version' => '3.1.2',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'flowbite/plugin' => 
+  array (
+    'version' => '3.1.2',
+    'dependencies' => 
+    array (
+      0 => 'mini-svg-data-uri',
+      1 => 'tailwindcss/plugin',
+      2 => 'tailwindcss/defaultTheme',
+      3 => 'tailwindcss/colors',
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'mini-svg-data-uri' => 
+  array (
+    'version' => '1.4.4',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'tailwindcss/plugin' => 
+  array (
+    'version' => '4.0.4',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'tailwindcss/defaultTheme' => 
+  array (
+    'version' => '4.0.4',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'tailwindcss/colors' => 
+  array (
+    'version' => '4.0.4',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+);
